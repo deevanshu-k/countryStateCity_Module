128a24811205ce3bacbd4b0565e05d5902c26c65
Deevanshu Kushwah <deevanshukushwah80@gmail.com>
2023-02-22 11:24:57 +0530

    v1.0.8 Build Created

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,6 +1,9 @@
 import { City } from "./interfaces/city.interface";
 import { Country } from "./interfaces/country.interface";
 import { smallState } from "./interfaces/smallState.interface";
+import { State } from "./interfaces/state.interface";
 export declare function getAllCountries(): Country[];
 export declare function getStatesByCountry(countryCode: string): smallState[];
 export declare function getCitiesByStateAndCountry(countryCode: string, stateCode: string): City[];
+export declare function getCountryDetailByCountryCode(CountryCode: string): Country;
+export declare function getStateDetailByStateAndCountry(StateCode: string, CountryCode: string): State;
